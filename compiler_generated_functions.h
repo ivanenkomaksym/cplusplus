3d5c36e0351994fce7e0cff31f622e689e2be292
Maksym Ivanenko <ivanenkomaxim@yahoo.com>
2015-04-13 19:17:39 +0200

    Adding compiler generated functions examples.

---
+++ compiler_generated_functions.h
@@ -0,0 +1,46 @@
+#ifndef COMPILER_GENERATED_FUNCTIONS_H
+#define COMPILER_GENERATED_FUNCTIONS_H
+
+using namespace std;
+
+/*
+  C++ 03:
+  1. default constructor (generated only if no constructor is declared by user)
+  2. copy constructor (generated only if no 5, 6 not declared)
+  3. copy assignment operator (generated only if no 5, 6 declared)
+  4. destructor
+
+  C++ 11:
+  1. default constructor (generated only if no constructor is declared by user)
+  2. copy constructor (generated only if no 3, 4, 5, 6 not declared)
+  3. copy assignment operator (generated only if no 2, 4, 5, 6 declared)
+  4. destructor
+  5. move constructor (generated only if 2, 3, 4, 6 not declared by user)
+  6. move assignment operator (generated only if 2, 3, 4, 5 not declared by user)
+  */
+
+class Dog
+{
+    // C++ 03:
+    Dog() {}
+    Dog(const Dog &) {}
+    Dog &operator = (const Dog &) {}
+    ~Dog() {}
+
+    // C++ 11:
+    Dog(Dog &&) {}
+    Dog &operator = (Dog &&) {}
+};
+
+class Frog
+{
+    Frog(int = 0) {}                  // default ctor
+    Frog(const Frog &, int = 0) {}    // copy ctor
+    Frog(Frog &&, int = 0) {}         // move ctor
+};
+
+class Cow   // 1, 2(deprecated), 4
+{
+    Cow &operator = (const Cow &) = delete;
+};
+#endif // COMPILER_GENERATED_FUNCTIONS_H
