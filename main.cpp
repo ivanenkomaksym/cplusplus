d4abb9e92ce77682c50652ce0cd0147cc3ca0c7e
unknown <miv@mykolaiv.domain.ompartners.com>
2015-04-15 09:41:31 +0300

    Adding C++11 features examples.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 #include "move_semantics.h"
 #include "literals.h"
 #include "compiler_generated_functions.h"
+#include "cplusplus11features.h"
 
 int main()
 {
     std::cout << "C++11 Advanced and Modern Examples" << std::endl;
     move_semantics();
     literals();
+    cplusplus11features();
     return 0;
 }
